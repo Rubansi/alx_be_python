dda60e327afda30b940270a05b4096ac5ea10e1c
rubansi <rubansi24@gmail.com>
2025-09-09 20:25:34 +0300

    performed the multiplication operation

--- basic_operations.py
+++ basic_operations.py
@@ -7,6 +7,10 @@ number2 = 5
 sum = number1 + number2
 print(f"Addition of {number1} and {number2} is {sum} ")
 
-# subtraction
+# subtraction operation
 difference = number1 - number2
 print(f"Subtraction of {number1} and {number2} is {difference} ")
+
+# multiplication operation
+product = number1 * number2
+print(f"Multiplication of {number1} and {number2} is {product} ")
