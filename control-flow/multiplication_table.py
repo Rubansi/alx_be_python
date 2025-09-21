39f919efc8280bf46cf012a6abae51f223a5e056
rubansi <rubansi24@gmail.com>
2025-09-21 12:13:49 +0300

    task submission

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,9 @@
+number = int(input("Enter a number to see its multiplication table: "))
+
+for int in range (10):
+    factor = (int+1) * number
+    print(f"{number}*{int + 1} = {factor}")
+   
+    
+    
+
