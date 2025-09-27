9fed0184d5050d1959ba269ee0fe29a8e4f97b89
rubansi <rubansi24@gmail.com>
2025-09-27 18:27:24 +0300

    task submission

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,7 +1,7 @@
 #accept inputs from user
-# num1 = float(input("Enter the first number: "))
-# num2 = float(input("Enter the second number: "))
-# operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
 def perform_operation(num1, num2, operation):
     if "add" in operation:
